143baa7b240b5ad9378540ec77abe78987b144d4
Cristian González <cristiang@empathy.co>
2020-08-27 17:19:59 +0200

    add representation symbol to the scope

--- src/main/java/rootcg/lum/core/definitions/Scope.java
+++ src/main/java/rootcg/lum/core/definitions/Scope.java
@@ -2,6 +2,16 @@ package rootcg.lum.core.definitions;
 
 public enum Scope {
 
-    PUBLIC, PRIVATE
+    PUBLIC('+'), PRIVATE('-');
+
+    char representation;
+
+    Scope(char representation) {
+        this.representation = representation;
+    }
+
+    public char getRepresentation() {
+        return representation;
+    }
 
 }
